234c292656ba64436e67a1e7078a14fc5bfa8894
Sairam Kaushik <sairamkaushik1@gmail.com>
2023-03-04 18:28:11 +0530

    updated required ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import AcademicPartners from "../components/AcademicPartners";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";  
