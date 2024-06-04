9500321fed2eb749f9ceffcde500128e48a1e8e7
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-06-04 14:33:51 +0530

    added SET logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
-import mr_logo from '../public/mrlogo1.png'
-import icaiccit_logo from '../public/icaiccit-2023 logo_updated.png'
+import Image from "next/image";
+import React from "react";
+import mr_logo from "../public/mrlogo1.png";
+import icaiccit_logo from "../public/icaiccit-2023 logo_updated.png";
+import set from "../public/set.jpeg";
 const Header = () => {
   return (
-    <div className='hidden md:block w-full h-20 bg-white'>
-        <div className='flex items-center justify-center gap-10 w-full h-full'>
-            <Image src={icaiccit_logo} alt='mr_logo' width={210} height={210} />
-            <Image src={mr_logo} alt='mr_logo' width={250} height={250} />
-        </div>
+    <div className="hidden md:block w-full h-20 bg-white">
+      <div className="flex items-center justify-center gap-10 w-full h-full">
+        <Image src={icaiccit_logo} alt="mr_logo" width={210} height={210} />
+        <Image src={mr_logo} alt="mr_logo" width={250} height={250} />
+        <Image src={set} alt="mr_logo" width={150} height={150} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
