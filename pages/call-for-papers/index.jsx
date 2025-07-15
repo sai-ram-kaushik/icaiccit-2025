51e88bad07e702083e833b27999c558bbc3f1627
sai-ram-kaushik <sairamkaushik1@gmail.com>
2025-07-15 22:06:13 +0530

    updated with the paper submission deadline

--- pages/call-for-papers/index.jsx
+++ pages/call-for-papers/index.jsx
@@ -53,7 +53,7 @@ const index = () => {
                     <tr className="text-xl">
                       <td>Paper Submission Deadline:</td>
                       <td>
-                        15<sup>th</sup> July, 2025
+                        30<sup>th</sup> July, 2025
                       </td>
                     </tr>
                     <tr className="text-xl">
