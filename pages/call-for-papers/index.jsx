7c4cf1d1acc800b33933e3c97c2459ddc463241a
sai-ram-kaushik <sairamkaushik1@gmail.com>
2025-08-18 20:49:46 +0530

    updated the important dates section

--- pages/call-for-papers/index.jsx
+++ pages/call-for-papers/index.jsx
@@ -53,25 +53,19 @@ const index = () => {
                     <tr className="text-xl">
                       <td>Paper Submission Deadline:</td>
                       <td>
-                        15<sup>th</sup> August, 2025 (Revised)
+                        30<sup>th</sup> August, 2025 (Revised)
                       </td>
                     </tr>
                     <tr className="text-xl">
                       <td>Acceptance Notification:</td>
                       <td>
-                        30<sup>th</sup> August, 2025 (Revised)
+                        31<sup>th</sup> September, 2025 (Revised)
                       </td>
                     </tr>
                     <tr className="text-xl">
                       <td>Camera Ready Paper Submission Deadline:</td>
                       <td>
-                        15<sup>th</sup> September, 2025 (Revised)
-                      </td>
-                    </tr>
-                    <tr className="text-xl">
-                      <td>Early-bird registration deadline:</td>
-                      <td>
-                        5<sup>th</sup> August, 2025
+                        25<sup>th</sup> September, 2025 (Revised)
                       </td>
                     </tr>
                     <tr className="text-xl">
